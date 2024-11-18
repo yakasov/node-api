7ab9a3dacb125384f4f6359b1f19cb1cc2075ebb
yakasov <yakasovian@gmail.com>
2024-11-18 13:07:10 +0000

    Force CORS more

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ app.get("/status", (req, res) => {
   res.send({ Status: "Running" });
 });
 app.use("/mec", mecRoutes);
+
+app.options("*", cors());
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+app.use(allowCrossDomain);
