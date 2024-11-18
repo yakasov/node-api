b29f9bf9c6373cb45646a3fc0dea30bebfe42d77
yakasov <yakasovian@gmail.com>
2024-11-18 20:11:54 +0000

    Use normal ports and redirect HTTP

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const certificate = fs.readFileSync('./sslcert/server.pem', 'utf8');
 const options = { cert: certificate, key: privateKey }
 
 const app = express();
-const PORT = 3000;
+const HTTP_PORT = 80;
+const HTTPS_PORT = 443;
 
 app.use(express.json());
 app.use(cors());
@@ -31,9 +32,22 @@ app._router.stack.forEach(function(r){
   }
 })
 
-httpServer.listen(PORT - 1, () => {
-  console.log(`HTTP listening on ${PORT - 1}.`)
-})
-httpsServer.listen(PORT, () => {
-  console.log(`HTTPS listening on ${PORT}.`)
+// Redirect all HTTP traffic to HTTPS
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+  next();
+});
+
+http.createServer((req, res) => {
+  const host = req.headers.host || 'jmcd.uk';
+  const redirectTo = `https://${host}${req.url}`;
+  res.writeHead(301, { Location: redirectTo });
+  res.end();
+}).listen(HTTP_PORT, () => {
+  console.log(`HTTP listening on ${HTTP_PORT} and redirecting to HTTPS.`);
+});
+httpsServer.listen(HTTPS_PORT, () => {
+  console.log(`HTTPS listening on ${HTTPS_PORT}.`)
 })
