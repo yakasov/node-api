0d146993b4e139dd70bee6fead029d491f6dc6ac
yakasov <yakasovian@gmail.com>
2024-11-18 13:16:07 +0000

    Add missing OPTIONS method

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use("/mec", mecRoutes);
 app.options("*", cors());
 var allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 };
