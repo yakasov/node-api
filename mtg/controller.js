0bebcf62305f6870a924a46cd161d187cfaf019d
yakasov <yakasovian@gmail.com>
2024-11-24 23:22:57 +0000

    Error handling for cache reading

--- mtg/controller.js
+++ mtg/controller.js
@@ -44,27 +44,32 @@ function saveCards(req, res) {
 
   const user = req.body.user;
   const newCards = req.body.cards;
+  const errors = [];
 
   Object.entries(newCards).forEach(([s, cs]) => {
     cs.forEach((c) => {
-      const sLower = s.toLowerCase();
-      const cId = cache[sLower][c].id;
+      try {
+        const sLower = s.toLowerCase();
+        const cId = cache[sLower][c].id;
 
-      if (!data[user]) {
-        data[user] = {};
-      }
+        if (!data[user]) {
+          data[user] = {};
+        }
 
-      if (!data[user][sLower]) {
-        data[user][sLower] = [];
-      }
+        if (!data[user][sLower]) {
+          data[user][sLower] = [];
+        }
 
-      if (!data[user][sLower].includes(cId)) {
-        data[user][sLower].push(cId);
+        if (!data[user][sLower].includes(cId)) {
+          data[user][sLower].push(cId);
+        }
+      } catch (e) {
+        errors.push({ s, c, e })
       }
     })
   })
 
-  res.status(200).send({ data });
+  res.status(200).send({ data, errors });
 }
 
 module.exports = {
