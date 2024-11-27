32e84cbce21b10829e44d7c72785c88c099df9aa
yakasov <yakasovian@gmail.com>
2024-11-27 19:01:11 +0000

    More tests

--- db/controller.js
+++ db/controller.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2/promise");
 
-const connection = mysql.createConnection({
+const conn = mysql.createConnection({
   user: "node",
   host: "localhost",
   database: "data"
@@ -8,14 +8,26 @@ const connection = mysql.createConnection({
 
 async function testConnection(req, res) {
   try {
-    const response = (await connection).execute(
-      "SELECT * FROM `data.test"
-    )
+    await createTableIfNotExists(123456);
+    const response = await getTables();
 
     res.status(200).send({ response });
   } catch (e) {
     res.status(500).send({ Status: e.message });
   }
+}
+
+async function getTables() {
+  const [fields, ] = await (await conn).execute("SHOW TABLES");
+  return fields.map((e) => e.Tables_in_data);
+}
+
+async function createTableIfNotExists(id) {
+  if ((await getTables()).includes(id)) {
+    return;
+  }
+
+  await (await conn).execute("CREATE TABLE `" + id +"` (`id` VARCHAR(255) NOT NULL, `owned` TINYINT(0), `set` VARCHAR(255) NOT NULL, PRIMARY KEY (`id`))")
 }
 
 module.exports = {
