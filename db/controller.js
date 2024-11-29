a92cbf12f852c9bcf3a2a051305aa7df40ecc05f
yakasov <yakasovian@gmail.com>
2024-11-29 22:33:19 +0000

    Fixess

--- db/controller.js
+++ db/controller.js
@@ -47,7 +47,7 @@ async function transferCache(req, res) {
 
   Object.keys(cache).forEach((set) => {
     Object.entries(cache[set]).forEach(([k, c]) => {
-      const query = `INSERT INTO cache VALUES (${f(c.canBeFoil)}, '${c.colours.join(",")}', '${c.flavour_text ?? ""}', ${f(c.foil)}, '${c.frameEffects.length ? c.frameEffects.join(",") : ""}', '${c.id}', '${c.image}', '${c.keywords.join(",")}', ${c.legal}, ${f(c.local)}, ${f(c.mana_cost)}, '${c.name}', '${k}', '${c.oracle_text}', ${c.power}, ${c.price}, ${c.price_foil}, '${c.rarity}', '${c.set}', '${c.set_name}', ${c.toughness}, '${c.type_line}', '${c.url}')`;
+      const query = `INSERT INTO cache VALUES (${f(c.canBeFoil)}, '${c.colours.join(",")}', '${c.flavour_text ?? ""}', ${f(c.foil)}, '${c.frameEffects && c.frameEffects.length ? c.frameEffects.join(",") : ""}', '${c.id}', '${c.image}', '${c.keywords.join(",")}', ${c.legal}, ${f(c.local)}, ${f(c.mana_cost)}, '${c.name}', '${k}', '${c.oracle_text}', '${c.power}', ${c.price}, ${c.price_foil}, '${c.rarity}', '${c.set}', '${c.set_name}', '${c.toughness}', '${c.type_line}', '${c.url}')`;
       console.log(query);
       return;
     })
