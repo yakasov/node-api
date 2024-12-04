a72a2b60c38db5a3dd7335704f44af5ae7043257
yakasov <yakasovian@gmail.com>
2024-12-04 17:43:00 +0000

    Add transferCache back lol

--- db/controller.js
+++ db/controller.js
@@ -35,6 +35,69 @@ async function createTableIfNotExists(id) {
   );
 }
 
+async function transferCache(req, res) {
+  function f(bool) {
+    if (bool) {
+      return 1;
+    }
+    return 0;
+  }
+  const cache = JSON.parse(
+    fs.readFileSync(`../bot-rewrite-3-js/resources/mtg/mtgCache.json`, "utf-8")
+  );
+  const cn = await conn;
+  Object.keys(cache).forEach((set) => {
+    Object.entries(cache[set]).forEach(async ([k, c]) => {
+      const query = "\
+        INSERT IGNORE INTO `cache` (\
+          can_be_foil, colours, flavour_text, foil, frame_effects,\
+          id, image, keywords, legal, local,\
+          mana_cost, `name`, oracle_text, power, price,\
+          price_foil, rarity, `set`, set_name, toughness,\
+          type_line, url\
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)\
+      ";
+      const values = [
+        c.canBeFoil ? 1 : 0,
+        c.colours ? c.colours.join(",") : "",
+        c.flavour_text || null,
+        c.foil ? 1 : 0,
+        c.frameEffects && c.frameEffects.length
+          ? c.frameEffects.join(",")
+          : null,
+        c.id || null,
+        c.image || null,
+        c.keywords ? c.keywords.join(",") : "",
+        c.legal ? 1 : 0,
+        c.local ? 1 : 0,
+        c.mana_cost ? c.mana_cost : null,
+        c.name || null,
+        k || null,
+        c.oracle_text || null,
+        c.power || null,
+        c.price || 0,
+        c.price_foil || 0,
+        c.rarity || null,
+        c.set || null,
+        c.set_name || null,
+        c.toughness || null,
+        c.type_line || null,
+        c.url || null,
+      ];
+      try {
+        await cn.execute(query, values);
+      } catch (err) {
+        console.error("Error executing query:", err);
+        console.error("Query:", query);
+        console.error("Values:", values);
+      }
+    });
+  });
+
+  res.status(200).send();
+}
+
 module.exports = {
+  transferCache,
   testConnection,
 };
--- db/routes.js
+++ db/routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const controller = require("./controller");
 
 router.get("/testConnection", controller.testConnection);
+router.get("/transferCache", controller.transferCache);
 
 module.exports = router;
